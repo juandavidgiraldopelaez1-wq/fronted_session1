ff08befdbda257af6840e987848319793e662647
Juan David Giraldo <juandavidgiraldopelaez1@gmail.com>
2026-09-02 17:31:04 -0500

    feat: cartas

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+console.log("Accedí al js");
+function saludoFuncion(){
+    console.log("Hola desde la función");
+}
+
+const SALUDO_FLECHA = () => {
+    console.log("Hola desde la función flecha");
+}
+
+const SALUDO_DOS = function(){
+ console.log("hola desde la función anónima");
+}
